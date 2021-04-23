ee50ffa7ddd9669b3737787275241e6229fad9d8
Jacob Rice <rice0296@umn.edu>
2021-04-23 14:57:24 -0500

    init/main-window-edits

--- src/MainWindow.cs
+++ src/MainWindow.cs
@@ -5,14 +5,15 @@ using UI = Gtk.Builder.ObjectAttribute;
 namespace GradeCalculator{
     class MainWindow : Window{
 
-        private readonly GradeManager gradeManager;
+        protected readonly GradeManager gradeManager; //holds the parent grade manager
 
         public MainWindow(GradeManager gradeManager) : this(new Builder("MainWindow.glade"), gradeManager) { }
 
         private MainWindow(Builder builder, GradeManager gradeManager) : base(builder.GetRawOwnedObject("MainWindow")){
             builder.Autoconnect(this);
             this.gradeManager = gradeManager;
-            DeleteEvent += Window_DeleteEvent;  
+            DeleteEvent += Window_DeleteEvent;
+
         }
 
         private void Window_DeleteEvent(object sender, DeleteEventArgs a){
